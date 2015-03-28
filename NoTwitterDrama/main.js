ab286b442e54be5304693e9bd1168c0660e89a1d
Magixxxx <autopildid@gmail.com>
2015-03-28 23:38:18 +0200

    Fixed width digits + leading 0s

--- NoTwitterDrama/main.js
+++ NoTwitterDrama/main.js
@@ -8,23 +8,34 @@ var timerId = setInterval(printTimeSince, 1000);
 
 function printTimeSince()
 {
+    //Split the time into 4 parts
     var currentTime = Date.now();
-    
     var secondsLeft = Math.abs(currentTime - START_TIME) / 1000;
-
     var days = Math.floor(secondsLeft / 86400);
     secondsLeft -= days * 86400;
-
     var hours = Math.floor(secondsLeft / 3600) % 24;
     secondsLeft -= hours * 3600;
-
     var minutes = Math.floor(secondsLeft / 60) % 60;
     secondsLeft -= minutes * 60;
-
     var seconds = Math.round(secondsLeft);
     
-    document.getElementById('day').getElementsByClassName('timedigit')[0].innerHTML = days;
-    document.getElementById('hrs').getElementsByClassName('timedigit')[0].innerHTML = hours;
-    document.getElementById('min').getElementsByClassName('timedigit')[0].innerHTML = minutes;
-    document.getElementById('sec').getElementsByClassName('timedigit')[0].innerHTML = seconds;
+    //Add leading zeroes if necessary
+    if (seconds < 10) seconds = "0" + seconds;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (hours < 10) hours = "0" + hours;
+    if (days < 10) days = "00" + days;
+    else if (days < 100) days = "0" + days;
+    else if (days > 999) days = "999";
+    
+    updateView(document.getElementById('day').getElementsByClassName('timedigit')[0], days.toString());
+    updateView(document.getElementById('hrs').getElementsByClassName('timedigit')[0], hours.toString());
+    updateView(document.getElementById('min').getElementsByClassName('timedigit')[0], minutes.toString());
+    updateView(document.getElementById('sec').getElementsByClassName('timedigit')[0], seconds.toString());
+}
+
+function updateView(el, text) {
+    el.innerHTML = "";
+    for (var i = 0; i < text.length; i++) {
+        el.innerHTML += '<span class="letter">'+ text[i] +'</span>';
+    }
 }
